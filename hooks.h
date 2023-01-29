33a2507dc4d7395c472d5c5daf329474e2b78958
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2023-01-29 15:17:51 -0500

    Create hooks.h

---
+++ hooks.h
@@ -0,0 +1,11 @@
+#include <Windows.h>
+#include <iostream>
+#include <TlHelp32.h>
+#include <string>
+#include <TlHelp32.h>
+#include <stdio.h>
+#include <locale>
+#include <codecvt>
+int main() {
+	//ONLY FOR GAMES THAT NEED HOOKS!
+}

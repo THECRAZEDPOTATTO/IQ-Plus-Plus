a7926dda9db3ae995aa9b59fbe3aa0510b3882b3
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2023-02-05 14:52:34 -0500

    Update Lightrender.h

--- Lightrender.h
+++ Lightrender.h
@@ -3,21 +3,24 @@
 #include <string>
 #include <vector>
 #include <cstdint>
-#ifdef __APPLE__
-#include <CUESDK/CUESDK.h>
-#else
 #include <CUESDK.h>
-#endif
 #include "games.h"
 #include <atomic>
 #include <thread>
 #include <vector>
 #include <unordered_set>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <cstdlib>
+
+#include <unordered_set>
 #include <cmath>
 //Lightrender.h
 class GTAV {       
 public:             
-    int wantedlevl = 0;//4
+    int wantedlevl = 5;//4
     int robbery = 0;//5
     int misson = 0;//2
     int health = 100;//5
@@ -26,13 +29,13 @@ class Minecraft {
 public:
     int cave = 0;//4
     int nether = 0;//4
-    int deepdark = 0;//5
+    int deepdark = 1;//5
     int health = 20;//5
     int rain = 0;//3
 };
 GTAV GTA;
 Minecraft MC;
-std::vector<CorsairLedColor> GTA5WgetAvailableKeys()
+std::vector<CorsairLedColor> GTA5WgetAvailableKeys()//for vesper and esex
 {
     std::unordered_set<int> colorsSet;
     for (int deviceIndex = 0, size = CorsairGetDeviceCount(); deviceIndex < size; deviceIndex++)
@@ -188,7 +191,7 @@ void GTA5MPULSERUNNER(int ms) {
     lightingThread.join();
 }
 //END OF GTANON
-//START OF GTADEATH
+//START OF GTADEATH 
 std::vector<CorsairLedColor> GTA5DgetAvailableKeys(){
 
     std::unordered_set<int> colorsSet;
@@ -215,7 +218,7 @@ std::vector<CorsairLedColor> GTA5DgetAvailableKeys(){
         }
         else
         {
-            colorsVector.push_back({ static_cast<CorsairLedId>(ledId), 0, 0, 255 });//LEFT OVER JUNK DONT DEL IT WILL BRAKE LEDS AND RGB 
+            colorsVector.push_back({ static_cast<CorsairLedId>(ledId), 0, 225, 255 });//LEFT OVER JUNK DONT DEL IT WILL BRAKE LEDS AND RGB 
         }
     }
     return colorsVector;
@@ -290,6 +293,88 @@ std::vector<CorsairLedColor> MCDDgetAvailableKeys() {
     }
     return colorsVector;
 }
+//MCDEATH
+std::vector<CorsairLedColor> MCDgetAvailableKeys() {
+
+    std::unordered_set<int> colorsSet;
+    for (int deviceIndex = 0, size = CorsairGetDeviceCount(); deviceIndex < size; deviceIndex++)
+    {
+        if (const auto ledPositions = CorsairGetLedPositionsByDeviceIndex(deviceIndex))
+        {
+            for (auto i = 0; i < ledPositions->numberOfLed; i++)
+            {
+                const auto ledId = ledPositions->pLedPosition[i].ledId;
+                colorsSet.insert(ledId);
+            }
+        }
+    }
+
+    std::vector<CorsairLedColor> colorsVector;
+    colorsVector.reserve(colorsSet.size());
+    int colorChoice = rand() % 2;
+    for (const auto& ledId : colorsSet)
+    {
+        if (colorChoice == 0)
+        {
+            colorsVector.push_back({ static_cast<CorsairLedId>(ledId), 0, 0, 255 });
+        }
+        else
+        {
+            colorsVector.push_back({ static_cast<CorsairLedId>(ledId), 0, 0, 255 });//LEFT OVER JUNK DONT DEL IT WILL BRAKE LEDS AND RGB 
+        }
+    }
+    return colorsVector;
+}
+
+void MCDperformPulseEffect(int waveDuration, std::vector<CorsairLedColor>& ledColorsVec, int frames)
+{
+    const auto timePerFrame = frames;
+    bool isBlue = true;
+    for (auto i = 0; i < frames; ++i)
+    {
+        if (isBlue)
+        {
+            for (auto& ledColor : ledColorsVec)
+                ledColor.g = 0, ledColor.b = 0, ledColor.r = 225;
+        }
+        else
+        {
+            for (auto& ledColor : ledColorsVec)
+                ledColor.r = 225, ledColor.b = 0, ledColor.g = 0;
+        }
+        isBlue = !isBlue;
+        CorsairSetLedsColorsAsync(static_cast<int>(ledColorsVec.size()), ledColorsVec.data(), nullptr, nullptr);
+        std::this_thread::sleep_for(std::chrono::milliseconds(timePerFrame));
+    }
+}
+void MCDPULSERUNNER(int ms) {
+    std::atomic_int waveDuration{ 500 };
+    std::atomic_bool continueExecution{ true };
+
+    auto colorsVector = MCDgetAvailableKeys();
+    if (colorsVector.empty()) {
+        exit(1);
+    }
+    std::thread lightingThread([&waveDuration, &continueExecution, &colorsVector] {
+        while (continueExecution) {
+            MCDperformPulseEffect(waveDuration.load(), colorsVector, 255);
+        }
+        });
+    while (continueExecution) {
+        if (MC.health < 1) {
+            std::cout << "";
+        }
+        else {
+            continueExecution = false;
+        }
+    }
+    lightingThread.join();
+}
+
+
+
+
+//END OF MCDEATH
 void MCDDperformWavyGradientEffect(int waveDuration, std::vector<CorsairLedColor>& ledColorsVec, int frames)
 {
     const auto timePerFrame = 2000; 
@@ -334,6 +419,7 @@ void MCDDPULSERUNNER(int ms) {
     lightingThread.join();
 }
 //END OF MCDD
+//START OF MCWATER
 
 
 
@@ -359,6 +445,11 @@ void MCDD() {
     MCDDperformWavyGradientEffect(2000, ledColorsVec, 700);
     MCDDPULSERUNNER(10);
 }
+void MCD() {
+    std::vector<CorsairLedColor> ledColorsVec = MCDgetAvailableKeys();
+    MCDperformPulseEffect(2000, ledColorsVec, 500);
+    MCDPULSERUNNER(10);
+}
 void Loading() {
    
 }
@@ -375,6 +466,9 @@ void qenderGTA() {
     GTA5ROB();
     GTA5wanted();
     GTA5Death();
+}
+void qenderMC() {
+    MCDD();
 }
 void roblox(int status) {
 
